023d95089e94588e63b2322e4ebba5c026196ecf
CalvinRodo <calvin.rodo@cds-snc.ca>
2023-03-18 23:24:53 -0400

    feat: first commit

---
+++ app.py
@@ -0,0 +1,47 @@
+import random
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+thing_to_build = [ 
+  "Create an api",
+  "Build a website",
+  "Write a web_app",
+  "Create a service",
+  "Write a library",
+  "Output something to the cli",
+  "Do something with sms",
+  "Do something with email",
+]
+
+theme_to_build = [
+  "movies",
+  "books",
+  "Paul Craig",
+  "music",
+  "food",
+  "games",
+  "AI",
+  "hackathons",
+  "goats",
+  "holidays",
+]
+
+cds_thing_to_use = [ 
+  "forms",
+  "notify",
+  "the design system"
+]
+
+
+
+def get_random_combinations():
+  return f"""{random.choice(thing_to_build)}
+Theme: {random.choice(theme_to_build)}
+Must use: {random.choice(cds_thing_to_use)}
+"""
+
+
+@app.route("/hello")
+def hello():
+    return render_template('hello.html', name = "Calvin")
